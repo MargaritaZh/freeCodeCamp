21dbbc327bee9f119dfd5af9d72c211fe4e67be9
MargaritaZh <margaritazhitkevich@gmail.com>
2023-11-21 23:58:16 +0300

    bracket notation to find the first character

---
+++ task15.js
@@ -0,0 +1,7 @@
+//Используйте скобочные обозначения, чтобы найти первый символ в lastNameпеременной и присвоить его firstLetterOfLastName.
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
