cee314de0b495c042077ee470171d13e89e0c309
MargaritaZh <margaritazhitkevich@gmail.com>
2023-11-21 23:51:11 +0300

    += operator

---
+++ task14.js
@@ -0,0 +1,5 @@
+//Установите someAdjective строку длиной не менее 3 символов и добавьте ее с myStrпомощью +=оператора.
+
+const someAdjective = "Hello";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
