73d05322e427656f2787b125d8493b7664212d6b
MargaritaZh <margaritazhitkevich@gmail.com>
2023-11-22 00:07:34 +0300

    Let's try to equate met with the third letter of the variable

---
+++ task17.js
@@ -0,0 +1,6 @@
+//Попробуем приравнять thirdLetterOfLastNameтретью букву переменной, lastNameиспользуя скобочные обозначения.
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
