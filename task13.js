66e6ba3848d2693ba52bc77d89889e19f416a87e
MargaritaZh <margaritazhitkevich@gmail.com>
2023-11-21 23:27:59 +0300

    escaped double quotes (\").

---
+++ task13.js
@@ -0,0 +1,3 @@
+/*Используйте обратную косую черту , чтобы присвоить переменной строку, myStrчтобы при выводе ее на консоль вы увидели:
+I am a "double quoted" string inside "double quotes". */
+var myStr = 'I am a "double quoted" string inside "double quotes".'; // Change this line
