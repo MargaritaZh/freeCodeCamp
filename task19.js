b12d4e0c648593ed5edd6bb80fb8e31b6c46795a
MargaritaZh <margaritazhitkevich@gmail.com>
2023-11-22 14:14:06 +0300

    Create a reusableFunction

---
+++ task19.js
@@ -0,0 +1,6 @@
+/*Создайте функцию с именем functionWithArgs, которая принимает два аргумента и выводит их сумму на консоль разработчика.
+Вызовите функцию с двумя числами в качестве аргументов.*/
+function functionWithArgs(number1, number2) {
+  console.log(number1 + number2);
+}
+functionWithArgs(2, 3);
