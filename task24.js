da05970433f2b992d2291973d46e2858b9536506
MargaritaZh <margaritazhitkevich@gmail.com>
2023-11-24 00:08:52 +0300

    Change the associated if/else if statements to switch statements

---
+++ task24.js
@@ -0,0 +1,23 @@
+//Change the associated if/else if statements to switch statements.
+//Измените связанные операторы if / else if на switch операторы.
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+}
