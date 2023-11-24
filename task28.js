7d758d3e1597e314eebebe155b42cee66a64e820
MargaritaZh <margaritazhitkevich@gmail.com>
2023-11-24 23:05:43 +0300

    For

---
+++ task28.js
@@ -0,0 +1,10 @@
+// For
+//Используйте for цикл, чтобы передать значения от 1 до 5 в myArray.
+
+// Setup
+const myArray = [];
+for (let i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
+
+// Only change code below this line
