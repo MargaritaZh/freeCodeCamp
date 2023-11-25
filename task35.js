95bbca180324aa234155d4a7a32fbf2737403c1a
MargaritaZh <margaritazhitkevich@gmail.com>
2023-11-25 15:32:47 +0300

    if, else if и else для проверки нескольких условий

--- task35.js
+++ task35.js
@@ -8,3 +8,16 @@ function checkEqual(a, b) {
 }
 
 checkEqual(1, 2);
+/*Следующая функция использует операторы if, else if и else для проверки нескольких условий:
+function findGreaterOrEqual(a, b) {
+  return (a === b) ? "a and b are equal" 
+    : (a > b) ? "a is greater" 
+    : "b is greater";
+}*/
+/*В checkSignфункции используйте несколько условных операторов (в соответствии с рекомендуемым форматом findGreaterOrEqual), чтобы проверить, является ли число положительным, отрицательным или нулевым. Функция должна вернуть positive, negativeили zero.
+ */
+function checkSign(num) {
+  return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+}
+
+checkSign(10);
