a7e0d574cc4c533b5f826f13ee27cb8b7ecca180
MargaritaZh <margaritazhitkevich@gmail.com>
2023-11-25 15:16:42 +0300

    parseInt()

---
+++ task34.js
@@ -0,0 +1,7 @@
+/*Функция parseInt() анализирует строку и возвращает целое число. 
+Задача: 
+Используйте parseInt()в convertToIntegerфункции, чтобы она преобразовывала входную строку strв целое число и возвращала его.*/
+function convertToInteger(str) {
+  return parseInt(str);
+}
+convertToInteger("56");
