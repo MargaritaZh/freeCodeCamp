9b4301e05a0c04d0d5b42e164d39f93724b934b4
MargaritaZh <margaritazhitkevich@gmail.com>
2023-11-27 00:51:30 +0300

    spread operator and then copying

---
+++ task39.js
@@ -0,0 +1,11 @@
+//Unpacking the arr1 using the spread operator and then copying those values to arr2
+/*Скопируйте все содержимое arr1в другой массив, arr2используя оператор распространения.*/
+
+const arr1 = ["JAN", "FEB", "MAR", "APR", "MAY"];
+let arr2;
+
+arr2 = [...arr1]; // Change this line
+
+console.log(arr2);
+
+//arr2=["JAN", "FEB", "MAR", "APR", "MAY"];
