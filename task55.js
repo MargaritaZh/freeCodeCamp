91ebe6bd00c6118a1d85bd4449b994f8e108bd90
MargaritaZh <margaritazhitkevich@gmail.com>
2023-11-30 22:47:53 +0300

    Обработайте выполненное обещание с помощью then

---
+++ task55.js
@@ -0,0 +1,25 @@
+//Обработайте выполненное обещание с помощью then
+/*Обработайте выполненное обещание с помощью then
+Промисы наиболее полезны, когда в вашем коде есть процесс, который занимает неизвестное количество времени (т. е. что-то асинхронное), часто это запрос к серверу. Когда вы делаете запрос к серверу, это занимает некоторое время, и после его завершения вы обычно хотите что-то сделать с ответом сервера. Этого можно достичь, используя thenметод. Метод thenвыполняется сразу после выполнения вашего обещания с помощью resolve. Вот пример:
+
+myPromise.then(result => {
+  
+});
+resultпроисходит из аргумента, переданного методу resolve.
+
+*/
+//ЗАДАЧА Добавьте then метод в свое обещание. Используйте resultв качестве параметра его функцию обратного вызова и войдите resultв консоль.
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+
+  if (responseFromServer) {
+    resolve("We got the data");
+  } else {
+    reject("Data not received");
+  }
+  makeServerRequest.then((result) => {
+    console.log(result);
+  });
+});
