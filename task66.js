167c5d011d88d4e91379322d1fc055728e0e4c00
MargaritaZh <margaritazhitkevich@gmail.com>
2023-12-03 14:32:04 +0300

    Сопоставьте один символ с несколькими возмож-ми

---
+++ task66.js
@@ -0,0 +1,4 @@
+//Сопоставьте один символ с несколькими возможностями
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[eaiou]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
